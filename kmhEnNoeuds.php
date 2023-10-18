ab21afddd735fbd121ae05cf3645e777c649328e
melisa-suciu <144272179+melisa-suciu@users.noreply.github.com>
2023-10-18 16:18:12 +0200

    Delete kmhEnNoeuds.php

--- kmhEnNoeuds.php
+++
@@ -1,6 +0,0 @@
-<?php	
-     echo "Saisir la vitesse en km/h : " ;					
-     $vitesseNoeuds = fgets( STDIN) ;
-     $vitesseKM = $vitesseNoeuds / 1.852; 
-     echo "Résultat de la convesion : " , $vitesseKM , " noeuds\n" ;
-?>
