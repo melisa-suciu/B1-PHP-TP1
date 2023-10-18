d8fdff4a9dc37f7d1c0926b507612c00c37ebc77
melisa-suciu <144272179+melisa-suciu@users.noreply.github.com>
2023-10-18 16:18:34 +0200

    Delete montantTotalGroupeAdultes.php

--- montantTotalGroupeAdultes.php
+++
@@ -1,7 +0,0 @@
-<?php
-     echo "Prix par adulte: 17€\n" ;
-     echo "Saisir le nombre des adultes : " ;
-     $MontantAdulte = rtrim(fgets (STDIN)) ;
-     $APayer = 17 * $MontantAdulte ;
-     echo "Pour " , $MontantAdulte , " adultes , faut payer : " , $APayer, "€\n" ;
-     
