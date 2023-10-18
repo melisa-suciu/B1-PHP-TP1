ee64a21718d30c21f232f55ffaa75b10f239155f
melisa-suciu <144272179+melisa-suciu@users.noreply.github.com>
2023-10-18 16:17:55 +0200

    Delete celsiusEnFahrenheit.php

--- celsiusEnFahrenheit.php
+++
@@ -1,6 +0,0 @@
-<?php
-     echo "Saisir la temperature en °C : " ;
-     $celsiusfahr = fgets( STDIN ) ;
-     $tempenfahr = 1.8 * $celsiusfahr + 32 ;
-     echo "Temperature en °F : " , $tempenfahr , "°F\n" ;
-?>
